775cc327a951a3f03f60a4b5b9b873ac5984c249
hlaf <henriquelindgren@gmail.com>
2018-11-24 10:35:41 +0000

    Update the show action

--- app.rb
+++ app.rb
@@ -51,8 +51,14 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    case @game.check_win_or_lose
+      when :win
+        redirect '/win'
+      when :lose
+        redirect '/lose'
+      else
+        erb :show
+    end
   end
   
   get '/win' do
